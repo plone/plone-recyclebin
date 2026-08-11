3fbf14305d358fec7ebe707d0ca7221ec9dcdea2
David Glick <david@glicksoftware.com>
2026-08-11 13:37:30 -0700

    fix docs

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -215,14 +215,6 @@ html_extra_path = [
     "robots.txt",
 ]
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [
-    "_static",
-]
-
-
 # -- Options for autodoc ----------------------------------------------------
 
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
